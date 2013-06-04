95dd7cddb563d5c1f428f2b2929c740e977fdffe
k1w1 <k1w1@k1w1.org>
2013-06-04 16:18:16 -0700

    * Handle creating integration fields for requirements.

--- lib/aha-api/resources/integration_fields.rb
+++ lib/aha-api/resources/integration_fields.rb
@@ -2,20 +2,27 @@ module AhaApi
   class Resource
     module IntegrationFields
 
-      def create_integration_field(feature_ref, integration, name, value, options={})
-        post("api/#{api_version}/features/#{feature_ref}/integrations/#{integration}/fields", 
+      def create_integration_field(resource_ref, integration, name, value, options={})
+        post("api/#{api_version}/#{resource_type(resource_ref)}/#{resource_ref}/integrations/#{integration}/fields", 
           :integration_field => options.merge({:name => name, :value => value}))
       end
 
-      def integration_field(feature_ref, integration, field_name, options={})
-        get("api/#{api_version}/features/#{feature_ref}/integrations/#{integration}/fields/#{field_name}", options)
+      def integration_field(resource_ref, integration, field_name, options={})
+        get("api/#{api_version}/#{resource_type(resource_ref)}/#{resource_ref}/integrations/#{integration}/fields/#{field_name}", options)
       end
 
       def search_integration_fields(integration, field_name, field_value, options={})
         get("api/#{api_version}/integrations/#{integration}/fields/#{field_name}/value/#{field_value}", options)
       end
 
-
+    protected
+      def resource_type(resource_ref)
+        if resource_ref =~ /-\d+-\d+$/
+          "requirements"
+        else
+          "features"
+        end
+      end
     end
   end
 end
