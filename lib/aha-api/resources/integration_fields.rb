52845d69e982b190698b74737e7dda7f6c257d9e
k1w1 <k1w1@k1w1.org>
2014-02-12 13:36:57 -0800

    * Changed API for creating integration fields so that initiatives are supported.

--- lib/aha-api/resources/integration_fields.rb
+++ lib/aha-api/resources/integration_fields.rb
@@ -2,28 +2,17 @@ module AhaApi
   class Resource
     module IntegrationFields
 
-      def create_integration_field(resource_ref, integration, name, value, options={})
-        post("api/#{api_version}/#{resource_type(resource_ref)}/#{resource_ref}/integrations/#{integration}/fields", 
+      def create_integration_field(resource_type, resource_id, integration, name, value, options={})
+        post("api/#{api_version}/#{resource_type}/#{resource_id}/integrations/#{integration}/fields", 
           :integration_field => options.merge({:name => name, :value => value}))
       end
 
-      def integration_field(resource_ref, integration, field_name, options={})
-        get("api/#{api_version}/#{resource_type(resource_ref)}/#{resource_ref}/integrations/#{integration}/fields/#{field_name}", options)
+      def integration_field(resource_type, resource_id, integration, field_name, options={})
+        get("api/#{api_version}/#{resource_type}/#{resource_id}/integrations/#{integration}/fields/#{field_name}", options)
       end
 
       def search_integration_fields(integration, field_name, field_value, options={})
         get("api/#{api_version}/integrations/#{integration}/fields/#{field_name}/value/#{field_value}", options)
-      end
-
-    protected
-      def resource_type(resource_ref)
-        if resource_ref =~ /-R-\d+$/ or resource_ref =~ /-R-PL$/
-          "releases"
-        elsif resource_ref =~ /-\d+-\d+$/
-          "requirements"
-        else
-          "features"
-        end
       end
     end
   end
