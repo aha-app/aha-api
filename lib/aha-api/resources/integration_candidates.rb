1578ca49c22ead43a903444428325ca00b473977
Toray Altas <toray.altas@gmail.com>
2017-09-07 02:05:53 -0700

    Added update for integration_candidates

--- lib/aha-api/resources/integration_candidates.rb
+++ lib/aha-api/resources/integration_candidates.rb
@@ -2,12 +2,16 @@ module AhaApi
   class Resource
     module IntegrationCandidates
 
+      def integration_candidates(options={})
+        get("api/#{api_version}/integration_candidates", options)
+      end
+
       def create_integration_candidate(options={})
         post("api/#{api_version}/integration_candidates", options)
       end
 
-      def integration_candidates(options={})
-        get("api/#{api_version}/integration_candidates", options)
+      def update_integration_candidate(id, options={})
+        put("api/#{api_version}/integration_candidates/#{id}", options)
       end
 
     end
